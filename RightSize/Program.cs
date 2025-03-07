80920fc4b8c8a108f9d38870adfe374a98f0a3dc
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-03-07 14:06:25 +0000

    RightSize Commit

---
+++ RightSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if (arg.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+                if (arg.Length > 3)
+                {
+                    Console.WriteLine(arg);
+                }
+
+            }
+        }
+    }
+}
