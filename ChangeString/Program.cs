9d52fe1eeca503d0b3d2109ac2b28804a54374f0
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-03-07 15:01:00 +0000

    Exercise 2 completed

---
+++ ChangeString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string phrase = Console.ReadLine();
+            string character = Console.ReadLine();
+
+            string modifiedPhrase = phrase.Replace(character, "x");
+            Console.WriteLine(modifiedPhrase);
+        }
+    }
+}
